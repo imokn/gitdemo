ee7f58278ccc38b352eb4577c6a39d04fd030353
imokn <1026342785@qq.com>
2020-03-22 21:23:41 +0800

    add class

--- text.py
+++ text.py
@@ -1,1 +1,25 @@
 #Description :For suffix operation
+class Stack:
+    def __init__(self):   
+        self.items = [] 
+ 
+    def push(self, item):        
+        self.items.append(item) 
+ 
+    def pop(self):      
+        if self.is_empty():             
+            raise Exception('stackIsEmpty')         
+        else:             
+            return self.items.pop() 
+ 
+    def peek(self):    
+        if self.is_empty():             
+            raise Exception('stackIsEmpty')         
+        else:             
+            return self.items[-1] 
+ 
+    def is_empty(self):      
+        return self.items == [] 
+ 
+    def size(self):       
+        return len(self.items)
